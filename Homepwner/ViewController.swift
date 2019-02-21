0174ab991c09c2fa13b55863d26cc3c9c37921c9
Guled Ali <guledali@guleds-macbook-pro.local>
2019-02-21 10:11:32 -0600

    Initial Commit

---
+++ Homepwner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homepwner
+//
+//  Created by Guled Ali on 2/21/19.
+//  Copyright © 2019 Guled Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
